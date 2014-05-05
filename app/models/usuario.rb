158d0e3bdd47e5f0dfbb77d1476c850aee838b68
rafaelbpcrespo <rafaelbpcrespo@gmail.com>
2014-05-05 13:55:44 -0300

    Corrigindo layout entrar e gerando confirmable devise

--- app/models/usuario.rb
+++ app/models/usuario.rb
@@ -1,7 +1,7 @@
 class Usuario < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable, :registerable, :confirmable
          :recoverable, :rememberable, :trackable, :validatable
 
   has_one :cliente
