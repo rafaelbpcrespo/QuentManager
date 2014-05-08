e8d954e07b136d407ea3147ac9a9cd7e269c3c71
rafaelbpcrespo <rafaelbpcrespo@gmail.com>
2014-05-07 22:35:17 -0300

    Criando tipo de usuario

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140506182404) do
+ActiveRecord::Schema.define(version: 20140508013432) do
 
   create_table "carnes", force: true do |t|
     t.string   "nome"
@@ -77,6 +77,7 @@ ActiveRecord::Schema.define(version: 20140506182404) do
     t.string   "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
+    t.string   "tipo"
   end
 
   add_index "usuarios", ["confirmation_token"], name: "index_usuarios_on_confirmation_token", unique: true
