26f4d3c299e5097c407633bf36c885eafbac7536
rafaelbpcrespo <rafaelbpcrespo@gmail.com>
2014-06-02 12:07:32 -0300

    Criando metodo para abreviar nome do cliente e arrumando index de clientes

--- app/models/cliente.rb
+++ app/models/cliente.rb
@@ -6,6 +6,10 @@ class Cliente < ActiveRecord::Base
     data = I18n.l(self.data_de_nascimento)
   end
 
+  def nome_abreviado
+    nomes = self.nome.split
+    return nomes[0] + " " + nomes[1]
+  end
   # def data_de_nascimento_str=(data_de_nascimento_str)
   #   self.data_de_nascimento = self.data_de_nascimento_str
   # end
