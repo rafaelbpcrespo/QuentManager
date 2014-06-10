4b296c77a7ca691aab412620b98d99f2d4f9d193
rafaelbpcrespo <rafaelbpcrespo@gmail.com>
2014-06-09 21:41:21 -0300

    Config email development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -23,6 +23,24 @@ QuentManager::Application.configure do
   config.active_record.migration_error = :page_load
 
 config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+c#onfig.action_mailer.default_url_options = { :host => 'http://siquitutes.herokuapp.com' }
+
+
+  #These settings are for the sending out email for active admin and consequently the   devise mailer
+  ActionMailer::Base.delivery_method = :smtp
+  ActionMailer::Base.perform_deliveries = true
+  ActionMailer::Base.raise_delivery_errors = true
+  ActionMailer::Base.smtp_settings = 
+  {
+
+    :address            => 'smtp.gmail.com',
+    :port               => 587,
+    :domain             => 'gmail.com', #you can also use google.com
+    :authentication     => :plain,
+    :user_name          => 'rafaelbpcrespo@gmail.com',
+    :password           => '17rafa12R'
+    :enable_starttls_auto => true
+}
 
   # Debug mode disables concatenation and preprocessing of assets.
   # This option may cause significant delays in view rendering with a large
