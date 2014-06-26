b9321e9674d6ab569d56192911f1b159568a5f9b
rafaelbpcrespo <rafaelbpcrespo@gmail.com>
2014-06-26 17:47:34 -0300

    Corrigindo controller

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -9,6 +9,6 @@ class RegistrationsController < Devise::RegistrationsController
   end
 
   def sign_up_params
-      params.require(:usuario).permit(:email, :password, :confirmation_password, :remember_me, :cliente_attributes => [ :usuario_id, :nome, :celular, :telefone, :ramal, :endereco, :complemento, :empresa_id, :sexo, :data_de_nascimento, :data_de_pagamento, :setor, :cargo, :cpf])
+      params.require(:usuario).permit(:email, :password, :confirmation_password, :remember_me, :cliente_attributes => [ :usuario_id, :nome, :celular, :telefone, :ramal, :endereco, :complemento, :empresa_id, :sexo, :data_de_nascimento, :data_de_pagamento, :setor, :cargo, :cpf, :rg, :numero, :bairro, :cidade, :telefone_empresa, :celular_empresa, :email_empresa])
     end
 end
