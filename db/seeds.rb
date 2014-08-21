679be4383c8ecd8623599ae39eadbf3b2532dda6
rafaelbpcrespo <rafaelbpcrespo@gmail.com>
2014-08-20 21:19:46 -0300

    Seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+e = Empresa.new(:nome => "Spassu", :telefone => "2227723456", :endereco => "Rua Cap. Luiz Belegard, 150", :observacao => "Prédio Espelhado, próximo a Casa Luiz Belegard")
+e.save!
+
+u = Usuario.new(:email => "rafaelbpcrespo@gmail.com", :password => "123456", :tipo => "Administrador", :confirmed_at => DateTime.now)
+u.cliente = Cliente.new(:nome => "Admin", :empresa_id => Empresa.first.id, :celular => "22999923456", :data_de_pagamento => "10", :cpf => "99999999999", :cargo => "Analista de Suporte", :setor => "TI", :sexo => "M", :data_de_nascimento => "17/12/1989", :telefone_empresa => "2227723456")
+u.save!
+
