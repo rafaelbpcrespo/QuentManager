92b47804a199af0396d6172e19296f3e89b227b2
rafaelbpcrespo <rafaelbpcrespo@gmail.com>
2014-09-12 15:58:59 -0300

    Criando mascaras para formulario

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,7 +18,10 @@
 
 jQuery(function($){
   $("#usuario_cliente_attributes_celular").mask("(99)99999-9999");
+  $("#usuario_cliente_attributes_celular_empresa").mask("(99)99999-9999");
+  $("#usuario_cliente_attributes_cpf").mask("999.999.999-99");
   $("#usuario_cliente_attributes_telefone").mask("(99)9999-9999");
+  $("#usuario_cliente_attributes_telefone_empresa").mask("(99)9999-9999");
   $("#usuario_cliente_attributes_data_de_nascimento").mask("99/99/9999");
   $("#cliente_data_de_nascimento").mask("99/99/9999");
   $("#empresa_telefone").mask("(99)9999-9999");
