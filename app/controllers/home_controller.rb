2d4e5854c6ba9e4839677aeb4ed4ecf050f14b7a
rafaelbpcrespo <rafaelbpcrespo@gmail.com>
2014-09-12 08:44:44 -0300

    Nao exigindo login na pagina inicial do sistema e criando metodo para home de usuario logado

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,10 +2,14 @@ class HomeController < ApplicationController
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery
-  before_action :authenticate_usuario!
+  before_action :authenticate_usuario!, :except => [:index]
 
   def index
     
+  end
+
+  def inicial
+
   end
 
   def relatorio_produtos
