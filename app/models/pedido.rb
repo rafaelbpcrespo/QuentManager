b59968e4ba7f4e00027ed58e1bae276cbad673ba
rafaelbpcrespo <rafaelbpcrespo@gmail.com>
2014-10-09 14:04:53 -0300

    Corrigindo para pgsql

--- app/models/pedido.rb
+++ app/models/pedido.rb
@@ -96,7 +96,7 @@ class Pedido < ActiveRecord::Base
 
   def self.vendas_do_mes
     valor = 0;
-    Pedido.find(:all, :conditions => ['(created_at > ?) & (situacao = "Confirmado") ', Time.now.beginning_of_month]).map { |p| valor = valor + p.valor}
+    Pedido.find(:all, :conditions => ['(created_at > ?) & (situacao == "Confirmado") ', Time.now.beginning_of_month]).map { |p| valor = valor + p.valor}
     valor
   end
 
