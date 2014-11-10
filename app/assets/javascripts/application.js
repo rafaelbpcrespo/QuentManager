0a865e07142d811abfbe46210eb5e2b4d0c50ee3
rafaelbpcrespo <rafaelbpcrespo@gmail.com>
2014-11-10 13:14:02 -0200

    Corrigindo DropDown

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,8 +10,8 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery-2.1.1
 //= require bootstrap
+//= require jquery.min
 //= require jquery.validate
 //= require jquery.maskedinput
 //= require jquery_ujs
