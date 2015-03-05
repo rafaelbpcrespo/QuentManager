823d1d721e10b3527544b533194bd7dd6cc6af34
rafaelbpcrespo <rafaelbpcrespo@gmail.com>
2015-03-04 21:30:25 -0300

    Exibindo unidade dos produtos

--- app/helpers/produtos_helper.rb
+++ app/helpers/produtos_helper.rb
@@ -1,2 +1,14 @@
 module ProdutosHelper
+  def unidade_medida(produto)
+    #debugger
+    if ["Lata","Copo","Pet 600ml", "Pet 2l", "Unidade", "Embalagem Individual", "Garrafa 500ml", "Garrafa 1l", "Caixa", "Pacote", "Sache"].include? produto.produto_tipo.nome
+      return "un"
+    elsif produto.produto_tipo.nome == "Kilo"
+      return "kg"
+    elsif produto.produto_tipo.nome == "Grama"
+      return "g"
+    elsif produto.produto_tipo.nome == "Litro"
+      return "l"
+    end
+  end
 end
